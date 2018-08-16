ab0fa99efe4323f41de1f2016341d09468fd1822
Marcelo Pereira <marcelopereiraIFSP@gmail.com>
2018-08-16 17:35:46 -0300

    First commit

---
+++ src/Components/FirstAnimate.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <transition name="fade">
+        <h1 v-if="show">hello</h1>
+      </transition>
+      <button @click="showMessage">Mostar Mensagem</button>
+    </div>
+    
+</template>
+
+<script>
+export default {
+   data () {
+    return {
+      msg: 'Hello',
+      show: false
+    }
+  },
+  methods: {
+      showMessage(){
+        this.show = !this.show
+      }
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
